af630c86446e788e15e8f054f59f0e0a7fac778b
yksl <yukselkurtuldu9@gmail.com>
2024-04-02 09:37:19 +0300

    del dropdown

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Doctors from '../components/Doctors';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 const Home = ({ doctorData }) => {
